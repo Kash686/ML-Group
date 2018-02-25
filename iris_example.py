80f3c60941cc8c472a4c315dd1ab73558e2faeb9
Prakash Balachandran <PBalachandran@PrakashdransMBP.home>
2018-02-25 18:44:38 -0500

    adding iris_example.py

---
+++ iris_example.py
@@ -0,0 +1,51 @@
+
+# coding: utf-8
+
+# In[42]:
+
+import pandas as pd
+from sklearn.decomposition import PCA
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+
+
+# In[2]:
+
+dat=pd.read_csv('/Users/PBalachandran/Dropbox/ML/data/iris.csv')
+
+
+# In[41]:
+
+pcaDat=dat[['sepal_length', 'sepal_width', 'petal_length','petal_width']]
+pcaClassVec=dat.species
+C=list(set(pcaClass))
+cl=['blue','red','green']
+
+pca=PCA(n_components=2)
+
+pca.fit(pcaDat)
+
+Z=pca.transform(pcaDat)
+
+for i in range(len(C)):
+    plt.scatter(Z[np.where(pcaClass==C[i]),0],Z[np.where(pcaClass==C[i]),1],c=cl[i])
+
+plt.legend(C)
+plt.show()
+
+
+# In[43]:
+
+LDA(pcaDat)
+
+
+# In[45]:
+
+LDA.fit(pcaDat,pcaClassVec)
+
+
+# In[ ]:
+
+
+
