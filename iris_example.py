d6857a7f87481bf45a5759aa28612a4090709df1
samcervantes <cervantes.samuel@gmail.com>
2018-02-25 19:14:34 -0500

    add plot-different-svm-classifiers-for-iris.py

--- iris_example.py
+++ iris_example.py
@@ -12,14 +12,14 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
 # In[2]:
 
-dat=pd.read_csv('/Users/PBalachandran/Dropbox/ML/data/iris.csv')
+dat=pd.read_csv('data/iris.csv')
 
 
 # In[41]:
 
 pcaDat=dat[['sepal_length', 'sepal_width', 'petal_length','petal_width']]
 pcaClassVec=dat.species
-C=list(set(pcaClass))
+C=list(set(pcaClassVec))
 cl=['blue','red','green']
 
 pca=PCA(n_components=2)
@@ -29,7 +29,7 @@ pca.fit(pcaDat)
 Z=pca.transform(pcaDat)
 
 for i in range(len(C)):
-    plt.scatter(Z[np.where(pcaClass==C[i]),0],Z[np.where(pcaClass==C[i]),1],c=cl[i])
+    plt.scatter(Z[np.where(pcaClassVec==C[i]),0],Z[np.where(pcaClassVec==C[i]),1],c=cl[i])
 
 plt.legend(C)
 plt.show()
